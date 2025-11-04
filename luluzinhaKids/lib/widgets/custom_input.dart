bd44886e3a6e174ae011002a714c39b3e4397cc4
bauertiago <tiagobauer@rede.ulbra.br>
2025-11-04 12:08:57 -0300

    refactor: update CustomInput to support auto-fill logic

--- luluzinhaKids/lib/widgets/custom_input.dart
+++ luluzinhaKids/lib/widgets/custom_input.dart
@@ -6,6 +6,10 @@ class CustomInput extends StatelessWidget {
   final Widget? suffixIcon;
   final bool obscureText;
   final TextInputType keyboardType;
+  final EdgeInsetsGeometry? contentPadding;
+  final TextEditingController? controller;
+  final ValueChanged<String>? onChanged;
+  final bool readOnly;
 
   const CustomInput({
     super.key,
@@ -14,18 +18,32 @@ class CustomInput extends StatelessWidget {
     this.suffixIcon,
     this.obscureText = false,
     this.keyboardType = TextInputType.text,
+    this.contentPadding,
+    this.controller,
+    this.onChanged,
+    this.readOnly = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
+      onChanged: onChanged,
       obscureText: obscureText,
       keyboardType: keyboardType,
+      readOnly: readOnly,
       decoration: InputDecoration(
         hintText: hintText,
         prefixIcon: Icon(prefixIcon),
         suffixIcon: suffixIcon,
         isDense: true,
+        contentPadding:
+            contentPadding ??
+            const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+        prefixIconConstraints: const BoxConstraints(
+          minHeight: 20,
+          minWidth: 40,
+        ),
       ),
     );
   }

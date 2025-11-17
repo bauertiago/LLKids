29e923f9bb3b8db525fe31c7764eb0e42b06ce0e
bauertiago <tiagobauer@rede.ulbra.br>
2025-11-17 11:03:11 -0300

    feat: Create a screen displaying products by category.

---
+++ luluzinhaKids/lib/screens/categoriesScreens/category_products_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:luluzinhakids/extensions/context_extensions.dart';
+import 'package:luluzinhakids/models/product.dart';
+import 'package:luluzinhakids/services/favorites_service.dart';
+
+import '../../widgets/custom_header.dart';
+import '../../widgets/custom_input.dart';
+import '../product_detail_screen.dart';
+
+class CategoryProductsScreen extends StatefulWidget {
+  final String title;
+  final List<Product> products;
+
+  const CategoryProductsScreen({
+    super.key,
+    required this.title,
+    required this.products,
+  });
+
+  @override
+  State<CategoryProductsScreen> createState() => _CategoryProductsScreenState();
+}
+
+class _CategoryProductsScreenState extends State<CategoryProductsScreen> {
+  final NumberFormat currencyFormat = NumberFormat.currency(
+    locale: 'pt_BR',
+    symbol: 'R\$',
+  );
+
+  final favoritesService = FavoritesService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CustomHeader(showBackButton: true, showLogo: true),
+            _buildSearchBar(),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(widget.title, style: context.texts.titleLarge),
+            ),
+            const SizedBox(height: 12),
+            Expanded(child: _buildGrid()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: CustomInput(
+        hintText: "Buscar produtos...",
+        prefixIcon: Icons.search,
+      ),
+    );
+  }
+
+  Widget _buildGrid() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: GridView.builder(
+        itemCount: widget.products.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 12,
+          childAspectRatio: 0.72,
+        ),
+        itemBuilder: (_, index) {
+          final item = widget.products[index];
+          return _buildProductCard(item);
+        },
+      ),
+    );
+  }
+
+  Widget _buildProductCard(Product item) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: Stack(
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: Image.asset(
+              item.nameImage,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+
+          Positioned(
+            top: 8,
+            right: 8,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  favoritesService.toggleFavorite(item);
+                });
+              },
+              child: Icon(
+                favoritesService.isFavorite(item)
+                    ? Icons.favorite
+                    : Icons.favorite_border,
+                color:
+                    favoritesService.isFavorite(item)
+                        ? Colors.red
+                        : Colors.white,
+                size: 26,
+              ),
+            ),
+          ),
+
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [Colors.black54, Colors.transparent],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    item.name,
+                    textAlign: TextAlign.center,
+                    style: context.texts.labelLarge,
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    currencyFormat.format(item.salePrice),
+                    style: context.texts.labelSmall,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ProductDetailScreen(product: item),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "COMPRAR >",
+                      textAlign: TextAlign.center,
+                      style: context.texts.labelMedium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

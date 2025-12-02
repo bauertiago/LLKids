97e1824cd15d1ca9183ffc0d8a6fa5461d4b7f7c
bauertiago <tiagobauer@rede.ulbra.br>
2025-12-02 18:13:40 -0300

    refactor: create reusable AppBar for admin_home_screen to remove duplicated code

--- luluzinhaKids/lib/screens/adminScreens/admin_home_screen.dart
+++ luluzinhaKids/lib/screens/adminScreens/admin_home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:luluzinhakids/extensions/context_extensions.dart';
 import 'package:luluzinhakids/screens/adminScreens/admin_add_product_screen.dart';
 
+import '../accessScreens/login_screen.dart';
 import 'admin_financial_screen.dart';
 import 'admin_products_screen.dart';
 
@@ -14,19 +17,39 @@ class AdminHomeScreen extends StatefulWidget {
 class _AdminHomeScreenState extends State<AdminHomeScreen> {
   int currentIndex = 0;
 
-  final screens = const [
+  final screens = [
     AdminProductsScreen(),
     AdminAddProductScreen(),
     AdminFinancialScreen(),
   ];
 
+  final titles = const ["Produtos", "Cadastrar Produto", "Financeiro"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(titles[currentIndex], style: context.texts.labelLarge),
+        backgroundColor: context.colors.primary,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.white),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => LoginScreen()),
+              );
+            },
+          ),
+        ],
+      ),
       body: screens[currentIndex],
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
+        unselectedItemColor: context.colors.secondary,
         onTap: (index) {
           setState(() {
             currentIndex = index;
